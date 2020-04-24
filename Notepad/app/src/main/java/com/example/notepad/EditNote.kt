4b61a6343e42794970c113ad8a820eb0dd20bcd3
Jurre Wolff <jurre.wolff@hva.nl>
2020-04-24 15:20:51 +0200

    Initial commit. Basic layout and imports.

---
+++ Notepad/app/src/main/java/com/example/notepad/EditNote.kt
@@ -0,0 +1,12 @@
+package com.example.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNote : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
